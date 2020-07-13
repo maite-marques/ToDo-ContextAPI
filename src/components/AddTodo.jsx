2734ef0fb8ecdda9b90eb74808f92ab32e09cc8c
Maitê <42325663+maite-marques@users.noreply.github.com>
2020-07-13 08:31:26 -0300

    fist commit

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useContext } from 'react';
+import { TodoContext } from '../contexts/TodoContext';
+
+const AddTodo = () => {
+  const { saveTodo } = useContext(TodoContext);
+  const[todo, setTodo] = useState();
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    saveTodo(todo);
+  }
+
+  const handleInputChange = e => {
+    setTodo({
+      ...todo,
+      title: e.target.value,
+    })
+  }
+
+  return(
+    <form onSubmit={handleFormSubmit}>
+      <input type="text" name="title" id="title" placeholder="Nova tarefa ..." onChange={handleInputChange} />
+      <button>Adicionar</button>
+    </form>
+  );
+}
+
+export default AddTodo;
